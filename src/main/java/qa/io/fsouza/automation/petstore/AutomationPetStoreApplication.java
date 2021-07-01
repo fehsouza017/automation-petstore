b160858b53ff3cbf3301f853985676f3d7970023
Danilo Barbosa <dansantty@gmail.com>
2021-07-01 13:54:42 -0300

    first-commit

---
+++ src/main/java/qa/io/fsouza/automation/petstore/AutomationPetStoreApplication.java
@@ -0,0 +1,13 @@
+package qa.io.fsouza.automation.petstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationPetStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationPetStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/qa/io/fsouza/automation/petstore/AutomationPetStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package qa.io.fsouza.automation.petstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationPetStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
